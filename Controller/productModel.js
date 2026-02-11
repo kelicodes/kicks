13363f80410d0148152f55df7ca90560f3b06604
kelicodes <kelidevske@gmail.com>
2026-02-11 09:38:01 +0300

    u

--- Controller/productModel.js
+++ Controller/productModel.js
@@ -1,5 +1,5 @@
 import Product from "../Models/productModel.js";
-import cloudinary from "../config/cloudinary.js";
+import cloudinary from "../Config/cloudinary.js";
 import fs from "fs";
 
 export const productUpload = async (req, res) => {
